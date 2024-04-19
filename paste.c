83685b0a96c0cb1da5ef3156d8a8ed0a915a79cc
DancinDragon <73773064+DancinDragon@users.noreply.github.com>
2024-04-19 17:28:12 +0530

    Update paste.c

--- paste.c
+++ paste.c
@@ -1,6 +1,12 @@
+#include<stdio.h>
 void paste()
 {
-    system("xclip -o >abc.text");
+    system("xclip -o >>abc.txt");
+    FILE *file = fopen("abc.txt","a");
+    if(file!= NULL){
+        fputs("\n",file);
+        fclose(file);
+    }
 }    
 
 int main(int argc, char *argv[]){
